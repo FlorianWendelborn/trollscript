6dc4b960aefc77503580c5d5235e62dca62b8ab0
FlorianWendelborn <florianwendgithub@googlemail.com>
2014-02-04 05:23:23 +0100

    changed syntax to trollscript

--- trollscript.js
+++ trollscript.js
@@ -6,16 +6,18 @@ var code = require('fs').readFileSync(process.argv[2])+"",
 	i = -1,
 	c = '';
 
-while (i++ < code.length) {
-	switch (code[i]) {
-		case '>':c+='ptr++;pptr[ptr]=(!pptr[ptr])?0:pptr[ptr];';break;
-		case '<':c+='ptr--;';break;
-		case '+':c+='++pptr[ptr];';break;
-		case '-':c+='--pptr[ptr];';break;
-		case '.':c+='process.stdout.write(String.fromCharCode(pptr[ptr]));';break;
-		case ',':c+='pptr[ptr]=input.charCodeAt(ii++)||0;';break;
-		case '[':c+='while(pptr[ptr] > 0){';break;
-		case ']':c+='}';
+while (i++*3 < code.length) {
+	switch ([code[i*3],code[i*3+1],code[i*3+2]].join('')) {
+		case 'Tro':break;
+		case 'ooo':c+='ptr++;pptr[ptr]=(!pptr[ptr])?0:pptr[ptr];';break;
+		case 'ool':c+='ptr--;';break;
+		case 'olo':c+='++pptr[ptr];';break;
+		case 'oll':c+='--pptr[ptr];';break;
+		case 'loo':c+='process.stdout.write(String.fromCharCode(pptr[ptr]));';break;
+		case 'lol':c+='pptr[ptr]=input.charCodeAt(ii++)||0;';break;
+		case 'llo':c+='while(pptr[ptr] > 0){';break;
+		case 'lll':c+='}';
+		case 'll.':break;
 	}
 }
 
