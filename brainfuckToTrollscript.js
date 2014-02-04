752fd834e2fafb824b2e4a6eed7e6729ad8b3ac8
FlorianWendelborn <florianwendgithub@googlemail.com>
2014-02-04 05:36:13 +0100

    added brainfuckToTrollscript generator

---
+++ brainfuckToTrollscript.js
@@ -0,0 +1,21 @@
+if (!process.argv[2]){console.log('no input file');process.exit(1)};
+if (!process.argv[3]){console.log('no output file');process.exit(1)};
+
+var file = require('fs').readFileSync(process.argv[2])+'';
+
+var o = 'Tro',i=0;
+while (i++ < file.length) {
+	switch (file[i]) {
+		case '>':o+='ooo';break;
+		case '<':o+='ool';break;
+		case '+':o+='olo';break;
+		case '-':o+='oll';break;
+		case '.':o+='loo';break;
+		case ',':o+='lol';break;
+		case '[':o+='llo';break;
+		case ']':o+='lll';break;
+	}
+}
+o+='ll.';
+
+require('fs').writeFileSync(process.argv[3],o);
